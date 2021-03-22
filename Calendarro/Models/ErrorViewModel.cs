36d608ded7597af414bdc9af4ad3876e6b26bb91
Isu <natantrojak@wp.pl>
2021-03-22 09:01:58 +0100

    Add project files.

---
+++ Calendarro/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Calendarro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
