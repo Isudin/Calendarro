206f85b9caeea7a1096508c085656e50597b7f18
Piotr T <tomaszekpiotrek@gmail.com>
2021-03-28 13:31:07 +0200

    Dodadnie systemu logowania

---
+++ Calendarro/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Calendarro.Areas.Identity.Data;
+using Calendarro.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Calendarro.Areas.Identity.IdentityHostingStartup))]
+namespace Calendarro.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                services.AddDbContext<LoginContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("LoginContextConnection")));
+
+                services.AddDefaultIdentity<CalendarroUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<LoginContext>();
+            });
+        }
+    }
+}
