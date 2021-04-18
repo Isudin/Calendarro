009e8f590e0cae3f1afcb933496de9c518f06eb2
Piotr T <tomaszekpiotrek@gmail.com>
2021-04-18 19:34:41 +0200

    Praca z kalendarzem v0.1

--- Calendarro/Controllers/CalendarController.cs
+++ Calendarro/Controllers/CalendarController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Calendarro.Models.Database;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,24 @@ namespace Calendarro.Controllers
 {
     public class CalendarController : Controller
     {
+        private readonly CalendarroDBContext _context;
+
+        public CalendarController(CalendarroDBContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        //public IActionResult GetAllTasks(int projectId)
+        //{
+        //    var tasks = _context.ProjectTasks
+        //        .Where(x => x.ProjectId == projectId).ToList();
+
+        //    return new JsonResult(tasks);
+        //}
     }
 }
