427bde31078930acb62bbf25c82debdc439f8a18
Isu <ntrojak98@gmail.com>
2021-05-10 15:38:46 +0200

    Dodano dto zadania

---
+++ Calendarro/Dto/TaskDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calendarro.Dto
+{
+    public class TaskDto
+    {
+        public int ProjectTaskId { get; set; }
+        public string TaskName { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? FinishDate { get; set; }
+        public int ProjectId { get; set; }
+        public int KanbanId { get; set; }
+        public int UserId { get; set; }
+    }
+}
