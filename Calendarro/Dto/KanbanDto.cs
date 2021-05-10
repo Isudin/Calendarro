b69a55f95da5a28c07d3ae3ec75a2d3ac79bbf9d
Isu <ntrojak98@gmail.com>
2021-05-10 15:35:42 +0200

    Dodano dto tablicy kanban

---
+++ Calendarro/Dto/KanbanDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calendarro.Dto
+{
+    public class KanbanDto
+    {
+        public int KanbanId { get; set; }
+        public int ProjectId { get; set; }
+        public string Name { get; set; }
+    }
+}
