ee38aeea33bda7a5f34498216e318789c2a30b3a
Isu <ntrojak98@gmail.com>
2021-05-10 16:07:05 +0200

    Dodano mapowanie encji do Dto

---
+++ Calendarro/Models/Mapper/Automap.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Calendarro.Models.Database;
+using Calendarro.Models.Dto;
+
+namespace Calendarro.Models.Mapper
+{
+    public class Automap : Profile
+    {
+        public Automap()
+        {
+            CreateMap<Projects, ProjectDto>();
+            CreateMap<CalendarroUsers, UserDto>();
+            CreateMap<Kanbans, KanbanDto>();
+            CreateMap<ProjectTasks, TaskDto>()
+                .ForMember(d => d.TaskId, source => source.MapFrom(s => s.ProjectTaskId));
+            CreateMap<ProjectUserRelation, RelationDto>()
+                .ForMember(d=> d.RelationId, source => source.MapFrom(s=>s.LinkId));
+        }
+    }
+}
