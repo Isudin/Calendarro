8e8e87d647c667c4d4a4e67f60ef0a422834de3b
Piotr T <tomaszekpiotrek@gmail.com>
2021-05-16 15:33:46 +0200

    Poprawa generowania wszystkich widokow oraz wyodrebnienie skryptu generujacego miesiac kalendarza

--- Calendarro/wwwroot/js/calendarViewTripple.js
+++ Calendarro/wwwroot/js/calendarViewTripple.js
@@ -3,8 +3,6 @@
     var today = new Date();
 
     for (let index = 1; index < 4; index++) {
-        debugger;
-
         var month = (((today.getMonth()) + index) % 12).toString();
         var year = today.getFullYear();
 
@@ -14,39 +12,6 @@
 
         var calendarDate = year + '-' + month + '-' + '01';
 
-        let calendar = new FullCalendar.Calendar(document.getElementById(`calendar${index}`), {
-            timeZone: 'UTC',
-            locale: 'pl',
-            initialDate: calendarDate,
-            themeSystem: 'bootstrap',
-            headerToolbar: {
-                left: '',
-                center: 'title',
-                right: ''
-            },
-            weekNumbers: false,
-            firstDay: '1',
-            fixedWeekCount: false,
-            selectable: true,
-            contentHeight: "auto",
-            handleWindowResize: true,
-            dateClick: function (info) {
-                $('#AddEvent').on('show.bs.modal', function (event) {
-                    var modal = $(this);
-
-                    modal.find('.modal-body #finish-date').val(info.dateStr)
-                });
-                //var dateValue = $(this).data(info.dateStr);
-                //$("#AddEvent .modal-body #dateValue").val(dateValue);
-                //$("#valDate").html(info.dateStr).modal('show');
-                //var text = $(info.dateStr).val();
-                //$("#modal_body").val(text);
-                $("#AddEvent").modal('show');
-            },
-            dayMaxEvents: false,
-            events: 'http://localhost:5000/calendar/getalltasks?project=1'
-        });
-
-        calendar.render();
+        generateMonth(index, calendarDate, "http://localhost:5000/calendar/getalltasks?project=1")
     }
 }
