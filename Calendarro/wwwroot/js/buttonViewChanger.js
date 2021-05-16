bbd123e386fc398e7d433a25bd3867eea0d6e639
Piotr T <tomaszekpiotrek@gmail.com>
2021-05-16 13:07:52 +0200

    Eksport gotowego skryptu zmieniajacego widoki

---
+++ Calendarro/wwwroot/js/buttonViewChanger.js
@@ -0,0 +1,23 @@
+﻿var button1 = document.getElementById('calendaView-1');
+var button2 = document.getElementById('calendaView-3');
+var button3 = document.getElementById('calendaView-6');
+
+var calContainer = document.getElementById('calendarContainer');
+
+button1.addEventListener('click', function () {
+    document.getElementById('singleView').style.display = 'block'
+    document.getElementById('trippleView').style.display = 'none'
+    document.getElementById('hexagonalView').style.display = 'none'
+});
+
+button2.addEventListener('click', function () {
+    document.getElementById('singleView').style.display = 'none'
+    document.getElementById('trippleView').style.display = 'block'
+    document.getElementById('hexagonalView').style.display = 'none'
+});
+
+button3.addEventListener('click', function () {
+    document.getElementById('singleView').style.display = 'none'
+    document.getElementById('trippleView').style.display = 'none'
+    document.getElementById('hexagonalView').style.display = 'block'
+});
