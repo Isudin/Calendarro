acb3f38d93c05f07e6d60d6c7e2dba62d214e1e9
Isu <ntrojak98@gmail.com>
2021-05-26 14:40:05 +0200

    Dodano projekt do testów

---
+++ CalendarroProjects/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalendarroTests
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void GetUserFromSession()
+        {
+        }
+    }
+}
