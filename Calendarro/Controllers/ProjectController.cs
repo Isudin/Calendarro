0b4fc182bd7922c97c1661858945ad44da164eeb
Piotr T <tomaszekpiotrek@gmail.com>
2021-05-30 12:07:30 +0200

    dodanie widoku do nowego projektu oraz poprawa w login partial na lsite

---
+++ Calendarro/Controllers/ProjectController.cs
@@ -0,0 +1,51 @@
+﻿using Calendarro.Models.Database;
+using Calendarro.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Calendarro.Models.Dto;
+
+namespace Calendarro.Controllers
+{
+    public class ProjectController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult AddNewProject()
+        {
+            return View(new NewProjectViewModel());
+        }
+
+        [HttpPost]
+        [ActionName("AddNewProject")]
+        public IActionResult AddNewProjectPost(NewProjectViewModel projectModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(nameof(AddNewProject), projectModel);
+            }
+
+            HttpContext.Session.TryGetValue("User", out var creator);
+
+            if (creator == null)
+            {
+                // blad bo nie ma uzytkownika w sesji
+            }
+
+            var options = new JsonSerializerOptions { WriteIndented = true };
+
+            var user = JsonSerializer.Deserialize<UserDto>(creator, options);
+
+            //var model = new Projects
+            //{
+            //    CreateDate = DateTime.Now,
+            //    Creator
+            //}
+
+            return View();
+        }
+    }
+}
---
+++ Calendarro/ViewModels/NewProjectViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Calendarro.ViewModels
+{
+    public class NewProjectViewModel
+    {
+        public string ProjectName { get; set; }
+        public DateTime FinishDate { get; set; }
+        public string Description { get; set; }
+    }
+}
