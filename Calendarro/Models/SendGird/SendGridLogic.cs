e5ae3ec7a6eda678ea5c05298887a703be20f22b
Flajusz <flajuniunio@gmail.com>
2021-06-12 19:02:50 +0200

    dodanie klasy SendGridLogic

---
+++ Calendarro/Models/SendGird/SendGridLogic.cs
@@ -0,0 +1,12 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+
+namespace Calendarro.Models.SendGird
+{
+    public class SendGridLogic
+    {
+
+    }
+}
