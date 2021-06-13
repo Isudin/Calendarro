28268ce8189028e1f403bfd5e6bf09c3b5bff62e
PawelR <pawlo97@gmail.com>
2021-06-13 11:33:09 +0200

    Poprawa testów jednostkowych

---
+++ Calendarro.Test/AppTests.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calendarro.ViewModels;
+
+namespace Test_PR
+{
+    [TestClass]
+    public class AppTests
+    {
+        private AddNewTaskViewModel _addNewTask;
+        private NewProjectViewModel _addNewProject;
+
+        [TestMethod]
+        public void NewTaskTest()
+        {
+            var res = false;
+            _addNewTask = new AddNewTaskViewModel()
+            {
+                Name = "nameoftask",
+                FinishDate = DateTime.Now,
+                Kanban = 10
+            };
+
+            if (_addNewTask != null)
+            {
+                res = true;
+            }
+
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void NewProjectTest()
+        {
+            var res = false;
+            var res2 = false;
+            _addNewProject = new NewProjectViewModel()
+            {
+                ProjectName = "nameofproject",
+                FinishDate = DateTime.Now,
+                Description = "Description for project"
+            };
+
+            if (_addNewProject != null )
+            {
+                res = true;
+                Assert.IsTrue(res);
+            }
+
+            if(_addNewProject.ProjectName.Length <= 20 && _addNewProject.ProjectName.Length >= 5)
+            {
+                res2 = true;
+                Assert.IsTrue(res2);
+            }
+        }
+    }
+}
